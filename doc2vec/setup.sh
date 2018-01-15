71a1275456cb201e6da6b445e7a1735bba82cfd2
Andrzej Dawidziuk <andrzej.dawidziuk@gmail.com>
2018-01-15 23:49:17 +0100

    Added doc2vec setup script

---
+++ doc2vec/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PATH="`pwd`/venv/bin:$PATH"
+if [ ! -d venv ]; then
+    virtualenv -p /usr/bin/python3 venv
+fi
+
+. venv/bin/activate
+
+echo "Installing requirements"
+
+pip install -r requirements.txt
+
+python -c "import nltk; nltk.download('reuters')"
+
+echo "Generating doc2vec model"
+
+python train.py model
+
+cat <<EOF
+
+Wejście do virtualenva:
+
+$ . venv/bin/activate
+
+Wyjście:
+
+$ deactivate
+
+Test gensima i nltk
+$ python test.py model 1
+
+EOF
+    
