1d0ea528af5e9cb680141056654101bb90215116
tkogowsk <hihahalo3>
2018-01-16 15:31:11 +0100

    Fetch similar from nlp.

--- AgentSystem/src/main/scala/HttpService/HttpService.scala
+++ AgentSystem/src/main/scala/HttpService/HttpService.scala
@@ -1,32 +1,45 @@
 package HttpService
 
-import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.Uri.Query
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
-import HttpMethods._
 import agents.ArtCompSystemApp
+import akka.actor.ActorSystem
 import akka.http.scaladsl.unmarshalling.Unmarshal
 
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import dbagent.dtos.ArticleDTO
+import dbagent.models.ArticleModel
+import dbagent.repository.ArticleRepository
+import enums.Tag
 import spray.json.DefaultJsonProtocol._
+import spray.json._
+import HttpMethods._
+import akka.util.ByteString
+
+import scala.collection.mutable.ListBuffer
 
 object HttpService {
 
-  implicit val system = ArtCompSystemApp.system
+  implicit val system = ActorSystem()
+
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
+
   final case class Vec(vector: List[Double])
+  final case class SimilarVectors(id: String, vector :Array[Double])
+  final case class SimilarVectorsWrapper(query_id: String, vectors :Array[JsValue])
+  final case class JsonWrapper(json: JsValue)
 
   implicit val vectorFormat = jsonFormat1(Vec)
+  implicit val similarVectorsFormat = jsonFormat2(SimilarVectors)
+  implicit val SimilarVectorsWrapperFormat = jsonFormat2(SimilarVectorsWrapper)
+  implicit val jsonWrapperFormat = jsonFormat1(JsonWrapper)
 
   def getVector(text: String): String = {
-
     val parameters = Map("text" -> text)
     var vector = ""
     Await.result({
@@ -41,6 +54,35 @@ object HttpService {
       }
     }, Duration.Inf)
     vector
+  }
+
+  def getSimilar(article :ArticleModel, articles: List[ArticleModel]): String = {
+    var vector = ""
+    var vectors = new ListBuffer[JsValue]
+    articles.foreach{elem => {
+      var list = new ListBuffer[Double]
+      elem.vector.split(",").foreach{ elem =>
+        list += elem.toDouble
+      }
+      vectors += SimilarVectors(elem.id.toString, list.toArray).toJson
+    }}
+
+    val parameters = SimilarVectorsWrapper(article.id.toString, vectors.toArray)
+    val json = JsonWrapper(parameters.toJson)
+    val entityJson = HttpEntity(ContentTypes.`application/json`, json.toJson.toString)
+    Await.result({
+      Http().singleRequest(HttpRequest(POST, uri = Uri("http://localhost:5000/similar"), entity = entityJson)).map {
+        value =>
+          var resp = None: Option[Vec]
+          println(value)
+      }
+    }, Duration.Inf)
+    vector
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a = ArticleRepository.getByTagName(Tag.POLITICS.name)
+    println(getSimilar(a.get.head, a.get))
   }
 
 }
