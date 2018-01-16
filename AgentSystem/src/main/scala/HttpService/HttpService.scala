5f59f8162963a6d430f5aec8367189fa41949763
tkogowsk <hihahalo3>
2018-01-16 19:37:25 +0100

    Fetch similarity.

--- AgentSystem/src/main/scala/HttpService/HttpService.scala
+++ AgentSystem/src/main/scala/HttpService/HttpService.scala
@@ -11,34 +11,33 @@ import akka.http.scaladsl.unmarshalling.Unmarshal
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import dbagent.dtos.ArticleDTO
 import dbagent.models.ArticleModel
-import dbagent.repository.ArticleRepository
-import enums.Tag
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 import HttpMethods._
-import akka.util.ByteString
+import dtos.SimilarityDTO
 
 import scala.collection.mutable.ListBuffer
 
 object HttpService {
 
-  //to run main method uncomment line below
-  //implicit val system = ActorSystem()
   implicit val system = ArtCompSystemApp.system
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
   final case class Vec(vector: List[Double])
-  final case class SimilarVectors(id: String, vector :Array[Double])
-  final case class SimilarVectorsWrapper(query_id: String, vectors :Array[JsValue])
+
+  final case class SimilarVectors(id: String, vector: Array[Double])
+
+  final case class SimilarVectorsWrapper(query_id: String, vectors: Array[JsValue])
+
   final case class JsonWrapper(json: JsValue)
 
   implicit val vectorFormat = jsonFormat1(Vec)
   implicit val similarVectorsFormat = jsonFormat2(SimilarVectors)
-  implicit val SimilarVectorsWrapperFormat = jsonFormat2(SimilarVectorsWrapper)
+  implicit val similarVectorsWrapperFormat = jsonFormat2(SimilarVectorsWrapper)
   implicit val jsonWrapperFormat = jsonFormat1(JsonWrapper)
+  implicit val similarityDtoFormat = jsonFormat2(SimilarityDTO)
 
   def getVector(text: String): String = {
     val parameters = Map("text" -> text)
@@ -57,16 +56,17 @@ object HttpService {
     vector
   }
 
-  def getSimilar(article :ArticleModel, articles: List[ArticleModel]): String = {
-    var vector = ""
+  def getSimilar(article: ArticleModel, articles: List[ArticleModel]): List[SimilarityDTO] = {
+    var similarity = new ListBuffer[SimilarityDTO]
     var vectors = new ListBuffer[JsValue]
-    articles.foreach{elem => {
+    articles.foreach { elem => {
       var list = new ListBuffer[Double]
-      elem.vector.split(",").foreach{ elem =>
+      elem.vector.split(",").foreach { elem =>
         list += elem.toDouble
       }
       vectors += SimilarVectors(elem.id.toString, list.toArray).toJson
-    }}
+    }
+    }
 
     val parameters = SimilarVectorsWrapper(article.id.toString, vectors.toArray)
     val json = JsonWrapper(parameters.toJson)
@@ -74,16 +74,16 @@ object HttpService {
     Await.result({
       Http().singleRequest(HttpRequest(POST, uri = Uri("http://localhost:5000/similar"), entity = entityJson)).map {
         value =>
-          var resp = None: Option[Vec]
-          println(value)
+          Await.result({
+            Unmarshal(value).to[List[(String, Double)]].map { results =>
+              results.foreach({ elem =>
+                similarity += SimilarityDTO(elem._1, elem._2)
+              })
+            }
+          }, Duration.Inf)
       }
     }, Duration.Inf)
-    vector
-  }
-
-  def main(args: Array[String]): Unit = {
-    val a = ArticleRepository.getByTagName(Tag.POLITICS.name)
-    println(getSimilar(a.get.head, a.get))
+    similarity.toList
   }
 
 }
---
+++ AgentSystem/src/main/scala/dtos/SimilarityDTO.scala
@@ -0,0 +1,7 @@
+package dtos
+
+final case class SimilarityDTO
+(
+  id: String,
+  similarity: Double
+)
